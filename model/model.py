8afbadf23ff78efc62814d1c8a14cadd084b7df1
DenisProcyon <denis.shadrin@procyon.ee>
2024-10-25 14:53:03 +0200

    added child and parent model class

--- model/model.py
+++ model/model.py
@@ -1,21 +1,49 @@
-from sklearn.linear_model import LogisticRegression
-
+from sklearn.linear_model import LogisticRegression, LinearRegression
 import pandas as pd
 import numpy as np
 
-class Model:
-    def __init__(self, feature_columns: list[str], target_column: str, model=LogisticRegression()):
-        self.__feature_columns = feature_columns
-        self.__target_column = target_column
+from abc import ABC, abstractmethod
+
+class Model(ABC):
+    def __init__(self, feature_columns: list[str], target_column: str, model):
+        self._feature_columns = feature_columns 
+        self._target_column = target_column
         self.model = model
-
+    
+    @abstractmethod
     def train(self, data: pd.DataFrame) -> None:
-        X = data[self.__feature_columns]
-        y = data[self.__target_column]
+        X = data[self._feature_columns]
+        y = data[self._target_column]
+        self.model.fit(X, y)
 
+    @abstractmethod
+    def predict(self, data: pd.DataFrame) -> np.ndarray:
+        pass
+
+class LogModel(Model):
+    def __init__(self, feature_columns: list[str], target_column: str, model=LogisticRegression()):
+        super().__init__(feature_columns, target_column, model)
+    
+    def train(self, data: pd.DataFrame) -> None:
+        X = data[self._feature_columns]
+        y = data[self._target_column]
         self.model.fit(X, y)
 
     def predict(self, data: pd.DataFrame) -> np.ndarray:
-        X = data[self.__feature_columns]
-        
-        return self.model.predict_proba(X)[:, 1]
+        X = data[self._feature_columns]
+
+        return self.model.predict_proba(X)[:, 1] 
+
+class LinearModel(Model):
+    def __init__(self, feature_columns: list[str], target_column: str, model=LinearRegression()):
+        super().__init__(feature_columns, target_column, model)
+
+    def train(self, data: pd.DataFrame) -> None:
+        X = data[self._feature_columns]
+        y = data[self._target_column]
+        self.model.fit(X, y)
+
+    def predict(self, data: pd.DataFrame) -> np.ndarray:
+        X = data[self._feature_columns]
+
+        return self.model.predict(X) 
